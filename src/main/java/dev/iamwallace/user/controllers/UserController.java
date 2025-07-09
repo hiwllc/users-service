482abe4933feb55b64a1143d88b3fb3a5d62ae98
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2025-07-09 05:23:09 -0300

    feat(users): add user register, get and delete

--- src/main/java/dev/iamwallace/user/controllers/UserController.java
+++ src/main/java/dev/iamwallace/user/controllers/UserController.java
@@ -2,21 +2,44 @@ package dev.iamwallace.user.controllers;
 
 import dev.iamwallace.user.business.UserService;
 import dev.iamwallace.user.business.dto.UserDTO;
+import dev.iamwallace.user.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
   private final UserService userService;
+  private final AuthenticationManager authenticationManager;
+  private final JwtUtil jwtUtil;
 
   @PostMapping
   public ResponseEntity<UserDTO> create(@RequestBody UserDTO userDTO) {
     return ResponseEntity.ok(userService.create(userDTO));
+  }
+
+  @PostMapping("/login")
+  public String login(@RequestBody UserDTO userDTO) {
+    Authentication authentication = authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(userDTO.getEmail(), userDTO.getPassword())
+    );
+
+    return jwtUtil.generateToken(authentication.getName());
+  }
+
+  @GetMapping
+  public ResponseEntity<UserDTO> getUserByEmail(@RequestParam("email") String email) {
+    return ResponseEntity.ok((userService.getUserByEmail(email)));
+  }
+
+  @DeleteMapping("/{email}")
+  public  ResponseEntity<Void> deleteUserByEmail(@PathVariable String email) {
+    userService.deleteUserByEmail(email);
+    return ResponseEntity.noContent().build();
   }
 }
